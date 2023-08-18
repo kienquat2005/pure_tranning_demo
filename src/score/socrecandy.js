c15cdb35e0eb9c3f1182f0ba54b7e25c460a6418
Vo Van Kiet <vokiet.15111999@gmail.com>
2023-08-18 22:45:54 +0700

    - update show point

--- src/score/socrecandy.js
+++ src/score/socrecandy.js
@@ -5,16 +5,20 @@ export class ScoreCandy extends Container{
     constructor(){
         super()
         this.scoreCandys = 1;
-        this.scoreCandy();
+        this.initScoreCandy()
+        this._initTween();
     }
-    scoreCandy(){
+    initScoreCandy(){
          this.scoreCandy = new Text("+" + this.scoreCandys,{
             fontSize : 50,
             fill: 0xFFD700,
 
         });
         this.addChild(this.scoreCandy);
-        let position = {x: this.scoreCandy.x, y: this.scoreCandy.y}
+        this.visible = false;
+    }
+
+    _initTween() {
         this.showTween = new Tween({y : this.scoreCandy.y}).to({y: -200}).onUpdate((obj)=>{
             this.scoreCandy.y = obj.y;
         });
@@ -22,18 +26,13 @@ export class ScoreCandy extends Container{
         this.alphaTween = new Tween({alpha : this.scoreCandy.alpha}).to({alpha : 0}).onUpdate((obj)=>{
             this.scoreCandy.alpha = obj.alpha;
         });
-
-        
-        // this.scaleTween= new Tween({scale : this.scoreCandy.scale}).to({scale : {x : 10, y : 10}}).onUpdate((obj)=>{
-        //     this.scoreCandy.scale.set(obj.scale.x, obj.scale.y);
-        // });
     }
 
     play(target){
         this.x = target.x;
         this.y = target.y;
+        this.visible = true;
         this.showTween.start();
         this.alphaTween.start();
-        // this.scaleTween.start();
     }
 }
